922ddd301602dc68833503394c3fe3c9391bfb5e
Nicolle Raphaël <raphael.nicolle@etu.unicaen.fr>
2024-02-06 08:08:51 +0100

    feat : adding a first css to the page

--- src/views/InputView.vue
+++ src/views/InputView.vue
@@ -20,11 +20,41 @@ function modifier(m){
 
 </script>
 <template>
-<div class="bg-gray-500">
-    <p v-for=" m in messages" :key="m">{{m}} <button @onclick="modifier(m)">Modif</button></p>
+<div id="page">
+    <div id="content" class="bg-gray-500">
+        <p v-for=" m in messages" :key="m">{{m}} <button @onclick="modifier(m)">Modif</button></p>
+    </div>
+    
+        <p id="nbMessage">{{nbMessages}}</p>
+        <div id="input-area">
+        <input id="messageInput" class="bg-gray-500 rounded-md" type="text" v-model=sendMessage width="20%">
+        <button @click="envoyerMessage()"><img src="/images/sendIcon.png" width="35px" alt=""></button>
+    </div>
 </div>
-<p>{{nbMessages}}</p>
-<input class="bg-gray-500 rounded-md" type="text" v-model="sendMessage">
-<button @click="envoyerMessage()"><img src="/images/sendIcon.png" width="50px" alt=""></button>
-
 </template>
+
+<style>
+#page{
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+}
+
+#content{
+    flex-grow: 1;
+}
+
+#nbMessage{
+    text-align: center;
+}
+
+#messageInput{
+    margin-right: 5%;
+}
+
+#input-area{
+    display: flex;
+    align-items: center;
+    align-self: center;
+}
+</style>
